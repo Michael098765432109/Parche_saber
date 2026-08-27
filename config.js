72545a5accb2cc9e5fad384a30f2c9eeb9bb13cd
michael <maicolhrnndz057@email.com>
2026-08-26 19:03:18 -0500

    Actualización del proyecto

---
+++ config.js
@@ -0,0 +1,4 @@
+window.PARCHE_SABER_CONFIG = Object.freeze({
+  supabaseUrl: 'https://qpofzkstajpfvbsuapdt.supabase.co',
+  supabasePublishableKey: 'sb_publishable_joFKoF_J22iwwuPpjOF32Q_-_gSWbiK'
+});
